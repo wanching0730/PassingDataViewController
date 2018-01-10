890d45dea4cea65ad2bca78df679be94cb1e1c7c
wanching0730 <wanching_97@hotmail.com>
2018-01-10 12:46:37 +0800

    Initial Commit

---
+++ PassingDataViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PassingDataViewController
+//
+//  Created by Wan Ching on 10/01/2018.
+//  Copyright © 2018 Wan Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
